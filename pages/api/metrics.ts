1fd9111d58ee84c1d4dc6a1134c573f69bcccc93
mymphe <azatsdev@gmail.com>
2021-11-11 13:17:46 +0600

    fix: use function decl for consistency

--- pages/api/metrics.ts
+++ pages/api/metrics.ts
@@ -3,10 +3,11 @@ import { register, collectDefaultMetrics } from 'prom-client'
 
 collectDefaultMetrics()
 
-const metrics = async (req: NextApiRequest, res: NextApiResponse) => {
+export default async function metrics(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   res.setHeader('Content-type', register.contentType)
   const defaultMetrics = await register.metrics()
   res.send(defaultMetrics)
 }
-
-export default metrics
