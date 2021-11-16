b6bf085db59f9f4387d2f9ad3abe654bd1688272
mymphe <azatsdev@gmail.com>
2021-11-16 17:20:32 +0600

    feat: do not output logs to file

--- modules/shared/utils/log.ts
+++ modules/shared/utils/log.ts
@@ -4,9 +4,5 @@ export const logger = createLogger({
   defaultMeta: {
     service: 'easy-track-ui',
   },
-  transports: [
-    new transports.Console({ format: winston.format.json() }),
-    new transports.File({ filename: 'easy-track-error.log', level: 'error' }),
-    new transports.File({ filename: 'easy-track-combined.log' }),
-  ],
+  transports: [new transports.Console({ format: winston.format.json() })],
 })
