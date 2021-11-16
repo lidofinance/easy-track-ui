217d505e7954c66a55cfb6395ec6f9d9827ab16f
mymphe <azatsdev@gmail.com>
2021-11-16 15:04:21 +0600

    feat: store logs in file

--- modules/shared/utils/log.ts
+++ modules/shared/utils/log.ts
@@ -4,5 +4,9 @@ export const logger = createLogger({
   defaultMeta: {
     service: 'easy-track-ui',
   },
-  transports: [new transports.Console({ format: winston.format.json() })],
+  transports: [
+    new transports.Console({ format: winston.format.json() }),
+    new transports.File({ filename: 'easy-track-error.log', level: 'error' }),
+    new transports.File({ filename: 'easy-track-combined.log' }),
+  ],
 })
