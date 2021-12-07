f196ec73d0ca713510bec9028345efd57e30747c
rezed <rezed93@gmail.com>
2021-12-07 14:27:10 +0300

    fix: transaction toast message

--- modules/blockChain/utils/sendTransactionGnosisWorkaround.ts
+++ modules/blockChain/utils/sendTransactionGnosisWorkaround.ts
@@ -3,7 +3,6 @@ import { PopulatedTransaction } from '@ethersproject/contracts'
 import { ToastInfo, toast } from '@lidofinance/lido-ui'
 import { ResultTx } from '../types'
 import { checkConnectedToSafe } from './checkConnectedToSafe'
-import { getWalletNameFromProvider } from './getWalletNameFromProvider'
 
 // This workaround exists because gnosis safe return making regular `sendTransaction` endlessly waiting
 // https://github.com/ethers-io/ethers.js/blob/7274cd06cf3f6f31c6df3fd6636706d8536b7ee2/packages/providers/src.ts/json-rpc-provider.ts#L226-L246
@@ -13,10 +12,9 @@ export async function sendTransactionGnosisWorkaround(
   transaction: PopulatedTransaction,
 ): Promise<ResultTx> {
   const provider = (signer.provider as any)?.provider
-  const walletName = getWalletNameFromProvider(provider)
   const isGnosisSafe = checkConnectedToSafe(provider)
 
-  const pendingToastId = ToastInfo(`Confirm transaction with ${walletName}`, {})
+  const pendingToastId = ToastInfo(`Confirm transaction in your wallet`, {})
 
   if (isGnosisSafe) {
     const hash: string = await (signer as any).sendUncheckedTransaction(
