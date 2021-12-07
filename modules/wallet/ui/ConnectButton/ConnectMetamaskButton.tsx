71bc4d72ca7ad10104e152d1b4582c7da8215b95
rezed <rezed93@gmail.com>
2021-12-07 18:29:47 +0300

    update: metamask button

--- modules/wallet/ui/ConnectButton/ConnectMetamaskButton.tsx
+++ modules/wallet/ui/ConnectButton/ConnectMetamaskButton.tsx
@@ -5,20 +5,17 @@ import { ConnectWalletButtonProps } from './types'
 import iconUrl from 'assets/icons/metamask.svg'
 
 export function ConnectMetamaskButton(props: ConnectWalletButtonProps) {
-  const { onConnect, disabled, ...rest } = props
+  const { onConnect, ...rest } = props
   const { connect } = useConnectorMetamask()
-  const isMetamaskInjected = !!(window as any).ethereum?.isMetaMask
 
   const handleConnect = useCallback(async () => {
-    if (!isMetamaskInjected) return
     onConnect?.()
     await connect()
-  }, [isMetamaskInjected, onConnect, connect])
+  }, [onConnect, connect])
 
   return (
     <ConnectButton
       {...rest}
-      disabled={!isMetamaskInjected || disabled}
       iconSrc={iconUrl}
       onClick={handleConnect}
       children="Metamask"
