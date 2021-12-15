5c0115fa573acaf3d5b624caa218987952e1eb69
mymphe <azatsdev@gmail.com>
2021-12-15 17:14:59 +0600

    feat(metrics): measure real ethereum requests

--- modules/network/utils/fetchWithFallback.ts
+++ modules/network/utils/fetchWithFallback.ts
@@ -1,13 +1,27 @@
+import { METRICS_PREFIX } from 'modules/shared/metrics/constants'
+import { Histogram } from 'prom-client'
+
 type FetchWithFallback = (
   inputs: RequestInfo[],
   init?: RequestInit | undefined,
 ) => Promise<Response>
 
+export const ethereumResponse = new Histogram({
+  name: METRICS_PREFIX + 'ethereum_response',
+  help: 'Ethereum response times',
+  labelNames: ['provider'],
+  buckets: [0.1, 0.2, 0.3, 0.6, 1, 1.5, 2, 5],
+})
+
 export const fetchWithFallback: FetchWithFallback = async (inputs, init) => {
   const [input, ...restInputs] = inputs
 
   try {
-    return await fetch(input, init)
+    const url = new URL(input as string)
+    const end = ethereumResponse.labels(url.hostname).startTimer()
+    const response = await fetch(input, init)
+    end()
+    return response
   } catch (error) {
     if (!restInputs.length) throw error
     return fetchWithFallback(restInputs, init)
--- modules/shared/metrics/index.ts
+++ modules/shared/metrics/index.ts
@@ -1,3 +1,4 @@
+import { ethereumResponse } from 'modules/network/utils/fetchWithFallback'
 import { Registry } from 'prom-client'
 import { buildInfo } from './buildInfo'
 import { chainInfo } from './chainInfo'
@@ -6,5 +7,6 @@ const registry = new Registry()
 
 registry.registerMetric(buildInfo)
 registry.registerMetric(chainInfo)
+registry.registerMetric(ethereumResponse)
 
 export { registry }
