55bcebba59f9855eb99490dc8a45466e1c224115
Azat Serikov <azatsdev@gmail.com>
2022-01-21 14:33:50 +0600

    feat(metrics): divide supported chains metrics into separate calls

--- modules/shared/metrics/chainInfo.ts
+++ modules/shared/metrics/chainInfo.ts
@@ -8,8 +8,14 @@ const { defaultChain, supportedChains } = publicRuntimeConfig
 export const chainInfo = new Gauge({
   name: METRICS_PREFIX + 'chain_info',
   help: 'Default chain and supported chains of the current build',
-  labelNames: ['default_chain', 'supported_chains'],
+  labelNames: ['default_chain', 'supported_chain'],
   registers: [],
 })
 
-chainInfo.labels(defaultChain, supportedChains).set(1)
+chainInfo.labels({ default_chain: defaultChain }).set(1)
+
+if (typeof supportedChains === 'string') {
+  supportedChains.split(',').forEach(chainId => {
+    chainInfo.labels({ supported_chain: chainId }).set(1)
+  })
+}
