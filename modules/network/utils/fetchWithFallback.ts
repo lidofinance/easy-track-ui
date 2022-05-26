cb7213bb49f5d4a819001b654bbf510c0b0a97d7
rezed <rezed93@gmail.com>
2022-05-26 18:38:35 +0400

    chore: codestyle

--- modules/network/utils/fetchWithFallback.ts
+++ modules/network/utils/fetchWithFallback.ts
@@ -16,12 +16,18 @@ export const ethereumResponse = new Histogram({
   registers: [],
 })
 
-export const fetchWithFallback: FetchWithFallback = async (inputs, chainId, init) => {
+export const fetchWithFallback: FetchWithFallback = async (
+  inputs,
+  chainId,
+  init,
+) => {
   const [input, ...restInputs] = inputs
 
   try {
     const url = new URL(input as string)
-    const end = ethereumResponse.labels(url.hostname, String(chainId)).startTimer()
+    const end = ethereumResponse
+      .labels(url.hostname, String(chainId))
+      .startTimer()
     const response = await fetch(input, init)
     end()
     return response
