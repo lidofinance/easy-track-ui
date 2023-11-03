bd25ea40f930d1e0604831e20deb847bb3c230b5
Alexander Belokon <ikatamarinaki@gmail.com>
2023-11-03 21:04:46 +0100

    refactor: extract useMotionTokenData to separate file

--- modules/motions/ui/MotionDescription/DescTopUpWithLimitsAndCustomToken.tsx
+++ modules/motions/ui/MotionDescription/DescTopUpWithLimitsAndCustomToken.tsx
@@ -8,10 +8,7 @@ import { AddressInlineWithPop } from 'modules/shared/ui/Common/AddressInlineWith
 import { formatUnits } from 'ethers/lib/utils'
 import { TopUpWithLimitsStablesAbi } from 'generated'
 import { NestProps } from './types'
-import { useSWR } from 'modules/network/hooks/useSwr'
-import { connectERC20Contract } from 'modules/motions/utils/connectTokenContract'
-import { useWeb3 } from 'modules/blockChain/hooks/useWeb3'
-import { DEFAULT_DECIMALS } from 'modules/blockChain/constants'
+import { useMotionTokenData } from 'modules/motions/hooks/useMotionTokenData'
 
 export function DescTopUpWithLimitsAndCustomToken({
   callData,
@@ -19,32 +16,10 @@ export function DescTopUpWithLimitsAndCustomToken({
 }: NestProps<TopUpWithLimitsStablesAbi['decodeEVMScriptCallData']> & {
   registryType: keyof typeof REGISTRY_WITH_LIMITS_BY_MOTION_TYPE
 }) {
-  const { chainId } = useWeb3()
   const { data: allowedRecipientMap, initialLoading: isRecipientDataLoading } =
     useRecipientMapAll({ registryType })
 
-  const { data: tokenData, initialLoading: isTokenDataLoading } = useSWR(
-    `top-up-with-limits-and-custom-token-${callData.token}-${chainId}`,
-    async () => {
-      try {
-        const tokenContract = connectERC20Contract(callData.token, chainId)
-        const label = await tokenContract.symbol()
-        const decimals = await tokenContract.decimals()
-
-        return {
-          label,
-          address: callData.token,
-          decimals,
-        }
-      } catch (error) {
-        return {
-          label: 'Unknown token',
-          address: callData.token,
-          decimals: DEFAULT_DECIMALS,
-        }
-      }
-    },
-  )
+  const { tokenData, isTokenDataLoading } = useMotionTokenData(callData.token)
 
   if (isRecipientDataLoading || !allowedRecipientMap || isTokenDataLoading) {
     return <div>Loading...</div>
@@ -63,7 +38,7 @@ export function DescTopUpWithLimitsAndCustomToken({
           <div key={i}>
             <b>{recipientName} </b>
             <AddressInlineWithPop address={address} /> with {formattedAmount}{' '}
-            {tokenData?.label ?? ''}
+            {tokenData ? <b>{tokenData.label}</b> : null}
           </div>
         )
       })}
