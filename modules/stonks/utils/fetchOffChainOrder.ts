0d80c922b86c9cec7d5abee0ba01f3eb868e7a23
Alexander Belokon <ikatamarinaki@gmail.com>
2024-04-11 16:13:16 +0200

    fix: use cow api directly

--- modules/stonks/utils/fetchOffChainOrder.ts
+++ modules/stonks/utils/fetchOffChainOrder.ts
@@ -2,13 +2,24 @@ import { CHAINS } from '@lido-sdk/constants'
 import { fetcherStandard } from 'modules/network/utils/fetcherStandard'
 import { OffChainOrder } from '../types'
 
+const COWSWAP_API_ENDPOINTS: Partial<Record<CHAINS, string | undefined>> = {
+  [CHAINS.Mainnet]: 'https://api.cow.fi/mainnet/api/v1',
+  [CHAINS.Goerli]: 'https://api.cow.fi/goerli/api/v1',
+}
+
 export const fetchOffChainOrder = async (
   orderAddress: string,
   chainId: CHAINS,
 ) => {
   try {
+    const cowApiEndpoint = COWSWAP_API_ENDPOINTS[chainId]
+
+    if (!cowApiEndpoint) {
+      throw new Error(`NO_CHAIN_${chainId}`)
+    }
+
     const ordersByOwner = await fetcherStandard<OffChainOrder[] | undefined>(
-      `/api/stonks/orders/${orderAddress}?chainId=${chainId}`,
+      `${cowApiEndpoint}/account/${orderAddress}/orders`,
     )
 
     if (!ordersByOwner?.length) {
