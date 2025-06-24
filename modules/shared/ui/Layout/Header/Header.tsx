b23417e4ba9aa4af55ac423d9c59b196c1fd970f
phonktown <ruslankhorolets@gmail.com>
2025-06-24 15:23:28 +0300

    feat(DG): add warning banner on top of the page

--- modules/shared/ui/Layout/Header/Header.tsx
+++ modules/shared/ui/Layout/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 import { useRouter } from 'next/dist/client/router'
 import { useWeb3 } from 'modules/blockChain/hooks/useWeb3'
 import { useScrollLock } from 'modules/shared/hooks/useScrollLock'
@@ -34,9 +34,12 @@ import { NoSSRWrapper } from 'modules/shared/ui/Utils/NoSSRWrapper'
 import StonksSVG from './icons/stonks.svg.react'
 import SettingsSVG from './icons/settings.svg.react'
 import { useAvailableStonks } from 'modules/stonks/hooks/useAvailableStonks'
-import { ButtonIcon } from '@lidofinance/lido-ui'
+import { ButtonIcon, Container } from '@lidofinance/lido-ui'
 import { DualGovernanceStatusButton } from 'modules/dualGovernance/DualGovernanceStatusButton'
 import { isTestnet } from 'modules/blockChain/utils/isTestnet'
+import { useDualGovernanceState } from 'modules/dualGovernance/useDualGovernanceState'
+import { DualGovernanceStatus } from 'modules/dualGovernance/types'
+import { DualGovernanceWarningBanner } from 'modules/dualGovernance/DualGovernanceWarningBanner'
 
 function NavItem({
   link,
@@ -63,122 +66,149 @@ function NavItem({
 export function Header() {
   const { chainId } = useWeb3()
   const [isBurgerOpened, setBurgerOpened] = useState(false)
+  const [showDualGovernanceWarningBanner, setShowDualGovernanceWarningBanner] =
+    useState(false)
+
   const handleCloseMobileMenu = useCallback(() => setBurgerOpened(false), [])
   useScrollLock(isBurgerOpened)
 
   const { areStonksAvailable } = useAvailableStonks()
 
+  const {
+    data: dualGovernanceStateData,
+    initialLoading: dualGovernanceStateInitialLoading,
+  } = useDualGovernanceState()
+
+  useEffect(() => {
+    if (!dualGovernanceStateData || dualGovernanceStateInitialLoading) {
+      return
+    }
+
+    if (
+      [
+        DualGovernanceStatus.VetoSignalling,
+        DualGovernanceStatus.RageQuit,
+        DualGovernanceStatus.VetoSignallingDeactivation,
+      ].includes(dualGovernanceStateData.status)
+    ) {
+      setShowDualGovernanceWarningBanner(true)
+    }
+  }, [dualGovernanceStateData, dualGovernanceStateInitialLoading])
+
   return (
     <>
-      <Wrap>
-        <Nav>
-          <Logo>
-            <LidoLogoSvg />
-          </Logo>
-          <NavItems>
-            <NavItem
-              link={urls.home}
-              icon={<ActiveMotionsSVG />}
-              children="Active motions"
-            />
-            <NavItem
-              link={urls.archive}
-              icon={<ArchiveSVG />}
-              children="Archive"
-            />
-            <NavItem
-              link={urls.startMotion}
-              icon={<StartSVG />}
-              children="Start motion"
-            />
-            {areStonksAvailable && (
-              <NavItem
-                link={urls.stonks}
-                icon={<StonksSVG />}
-                children="Stonks"
-              />
-            )}
-            {/* <NavItem link={urls.about} icon={<InfoSVG />} children="About" /> */}
-          </NavItems>
-        </Nav>
-
-        <ActionsDesktop>
-          <Network>
-            <NetworkBulb color={getChainColor(chainId)} />
-            <Text size={14} weight={500}>
-              {getChainName(chainId)}
-            </Text>
-          </Network>
-          <NoSSRWrapper>
-            {isTestnet(chainId) && <DualGovernanceStatusButton />}
-            <HeaderWallet />
-            <Link passHref href={urls.settings}>
-              <ButtonIcon
-                icon={<SettingsSVG />}
-                size="sm"
-                variant="outlined"
-                color="primary"
-              />
-            </Link>
-          </NoSSRWrapper>
-        </ActionsDesktop>
-
-        <BurgerWrap
-          isOpened={isBurgerOpened}
-          onClick={() => setBurgerOpened(!isBurgerOpened)}
-        >
-          <BurgerLine />
-          <BurgerLine />
-          <BurgerLine />
-        </BurgerWrap>
-
-        {isBurgerOpened && (
-          <>
-            <MobileNavItems>
+      {showDualGovernanceWarningBanner && <DualGovernanceWarningBanner />}
+      <Container as="header" size="full">
+        <Wrap>
+          <Nav>
+            <Logo>
+              <LidoLogoSvg />
+            </Logo>
+            <NavItems>
               <NavItem
                 link={urls.home}
                 icon={<ActiveMotionsSVG />}
                 children="Active motions"
-                onClick={handleCloseMobileMenu}
               />
               <NavItem
                 link={urls.archive}
                 icon={<ArchiveSVG />}
                 children="Archive"
-                onClick={handleCloseMobileMenu}
               />
               <NavItem
                 link={urls.startMotion}
                 icon={<StartSVG />}
                 children="Start motion"
-                onClick={handleCloseMobileMenu}
               />
               {areStonksAvailable && (
                 <NavItem
                   link={urls.stonks}
                   icon={<StonksSVG />}
                   children="Stonks"
-                  onClick={handleCloseMobileMenu}
                 />
               )}
-            </MobileNavItems>
+              {/* <NavItem link={urls.about} icon={<InfoSVG />} children="About" /> */}
+            </NavItems>
+          </Nav>
 
-            <MobileMenu>
-              <MobileMenuScroll>
-                <MobileNetworkWrap>
-                  <Network>
-                    <NetworkBulb color={getChainColor(chainId)} />
-                    <Text size={14} weight={500}>
-                      {getChainName(chainId)}
-                    </Text>
-                  </Network>
-                  <HeaderWallet />
-                </MobileNetworkWrap>
-              </MobileMenuScroll>
-            </MobileMenu>
-          </>
-        )}
-      </Wrap>
+          <ActionsDesktop>
+            <Network>
+              <NetworkBulb color={getChainColor(chainId)} />
+              <Text size={14} weight={500}>
+                {getChainName(chainId)}
+              </Text>
+            </Network>
+            <NoSSRWrapper>
+              {isTestnet(chainId) && <DualGovernanceStatusButton />}
+              <HeaderWallet />
+              <Link passHref href={urls.settings}>
+                <ButtonIcon
+                  icon={<SettingsSVG />}
+                  size="sm"
+                  variant="outlined"
+                  color="primary"
+                />
+              </Link>
+            </NoSSRWrapper>
+          </ActionsDesktop>
+
+          <BurgerWrap
+            isOpened={isBurgerOpened}
+            onClick={() => setBurgerOpened(!isBurgerOpened)}
+          >
+            <BurgerLine />
+            <BurgerLine />
+            <BurgerLine />
+          </BurgerWrap>
+
+          {isBurgerOpened && (
+            <>
+              <MobileNavItems>
+                <NavItem
+                  link={urls.home}
+                  icon={<ActiveMotionsSVG />}
+                  children="Active motions"
+                  onClick={handleCloseMobileMenu}
+                />
+                <NavItem
+                  link={urls.archive}
+                  icon={<ArchiveSVG />}
+                  children="Archive"
+                  onClick={handleCloseMobileMenu}
+                />
+                <NavItem
+                  link={urls.startMotion}
+                  icon={<StartSVG />}
+                  children="Start motion"
+                  onClick={handleCloseMobileMenu}
+                />
+                {areStonksAvailable && (
+                  <NavItem
+                    link={urls.stonks}
+                    icon={<StonksSVG />}
+                    children="Stonks"
+                    onClick={handleCloseMobileMenu}
+                  />
+                )}
+              </MobileNavItems>
+
+              <MobileMenu>
+                <MobileMenuScroll>
+                  <MobileNetworkWrap>
+                    <Network>
+                      <NetworkBulb color={getChainColor(chainId)} />
+                      <Text size={14} weight={500}>
+                        {getChainName(chainId)}
+                      </Text>
+                    </Network>
+                    <HeaderWallet />
+                  </MobileNetworkWrap>
+                </MobileMenuScroll>
+              </MobileMenu>
+            </>
+          )}
+        </Wrap>
+      </Container>
       <MobileSpacer />
     </>
   )
